e9e10f87ba2415969b907cf4ccefb07962152954
CovarrubiasAaron <145801340+CovarrubiasAaron@users.noreply.github.com>
2023-11-28 11:45:24 -0800

    Summary
    I coded to ruby to have health bar and be and be able to collect heals to heal herself.

--- P3Ruby'sAdventure/Assets/Scripts/Ruby Controller.cs
+++ P3Ruby'sAdventure/Assets/Scripts/Ruby Controller.cs
@@ -5,6 +5,11 @@ using UnityEngine.UIElements;
 
 public class RubyController : MonoBehaviour
 {
+    public float speed = 3.0f;
+    public int maxHealth = 5;
+    int currentHealth;
+
+    public int health;
     Rigidbody2D rigidbody2d;
     float horizontal;
     float vertical;
@@ -13,8 +18,10 @@ public class RubyController : MonoBehaviour
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
+        
     }
-
+    // Update is called once per frame 
     void Update()
     {
         horizontal = Input.GetAxis("Horizontal");
@@ -30,5 +37,10 @@ public class RubyController : MonoBehaviour
         position.y = position.y + 3.0f * vertical * Time.deltaTime;
 
         rigidbody2d.MovePosition(position);
+    }
+   public void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth = amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
     }
 }
